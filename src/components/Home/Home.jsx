5c05ab5fdb838f7efe22ae3540ddcd211f1f848b
prateekmishra1303 <prateekmishra1303@gmail.com>
2025-07-27 06:19:38 -0700

    Improving overlapping image and text content in home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -15,9 +15,8 @@ export default function Home() {
                             <span className="block text-3xl sm:text-4xl mt-2 sm:mt-0">Developer at TCS</span>
                         </h2>
 
-                        <Link
-                            href="https://github.com/your-username/your-project"
-                            to="https://docs.google.com/document/d/1_Ru-zvT1s_QRwP3kp69HcSXsdrXrqv_K93gOvJzt0X0/edit?usp=sharing"
+                        <a
+                            href="https://docs.google.com/document/d/1_Ru-zvT1s_QRwP3kp69HcSXsdrXrqv_K93gOvJzt0X0/edit?usp=sharing"
                             target="_blank"
                             rel="noopener noreferrer"
                             className="inline-flex text-white items-center px-6 py-3 font-medium bg-orange-700 rounded-lg hover:opacity-80 transition"
@@ -34,12 +33,15 @@ export default function Home() {
                                 <path d="M1.571 23.664l10.531-10.501 3.712 3.701-12.519 6.941c-.476.264-1.059.26-1.532-.011l-.192-.13zm9.469-11.56l-10.04 10.011v-20.022l10.04 10.011zm6.274-4.137l4.905 2.719c.482.268.781.77.781 1.314s-.299 1.046-.781 1.314l-5.039 2.793-4.015-4.003 4.149-4.137zm-15.854-7.534c.09-.087.191-.163.303-.227.473-.271 1.056-.275 1.532-.011l12.653 7.015-3.846 3.835-10.642-10.612z" />
                             </svg>
                             &nbsp; Projects
-                        </Link>
+                        </a>
                     </div>
                 </div>
 
-                <div className="absolute inset-0 w-full pt-10 sm:pt-20 sm:my-16">
-                    <img className="w-64 sm:w-96 object-contain" src="https://www.animationxpress.com/wp-content/uploads/2015/01/common-man.jpg" alt="image1" />
+                <div className="flex justify-center sm:absolute sm:inset-0 sm:pt-20">
+                    <img className="w-64 sm:w-96 object-contain" 
+                    src="https://www.animationxpress.com/wp-content/uploads/2015/01/common-man.jpg" 
+                    alt="image1" 
+                    />
                 </div>
             </aside>
 
